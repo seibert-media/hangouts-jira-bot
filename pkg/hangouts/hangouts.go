8d30c32823d2bd8ccecec2e567c9bddb04deedcb
Kevin Wiesmüller <kwiesmueller@seibert-media.net>
2018-05-08 21:42:18 +0200

    add webhook functionality

--- pkg/hangouts/hangouts.go
+++ pkg/hangouts/hangouts.go
@@ -3,6 +3,7 @@ package hangouts
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -16,6 +17,7 @@ import (
 type Hangouts struct {
 	*log.Logger
 	*http.Client
+	URL string
 }
 
 // New Hangouts client
@@ -33,19 +35,31 @@ func New(ctx context.Context, log *log.Logger, serviceAccount string) (*Hangouts
 	}, nil
 }
 
+// NewWebhookClient for Hangouts
+func NewWebhookClient(log *log.Logger, url string) (*Hangouts, error) {
+	return &Hangouts{
+		Logger: log,
+		Client: &http.Client{},
+		URL:    url,
+	}, nil
+}
+
 // SendCard to Hangouts
-func (h *Hangouts) SendCard(space string, card string) error {
+// space can be left empty if using webhooks
+func (h *Hangouts) SendCard(space string, card *Card) error {
 	/*msg := Message{
 		Cards: []Card{
 			card,
 		},
 	}*/
-	url := fmt.Sprintf("https://chat.googleapis.com/v1/%s/messages", space)
-	//data, err := json.Marshal(msg)
-	//if err != nil {
-	//	return err
-	//}
-	resp, err := h.Client.Post(url, "application/json", bytes.NewBuffer([]byte(card)))
+	if h.URL == "" {
+		h.URL = fmt.Sprintf("https://chat.googleapis.com/v1/%s/messages", space)
+	}
+	data, err := json.Marshal(card)
+	if err != nil {
+		return err
+	}
+	resp, err := h.Client.Post(h.URL, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		return err
 	}
