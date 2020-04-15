e79c10c1ec0f639c2b83579f8ab045f766d141ba
Kevin Wiesmueller <kwiesmueller@seibert-media.net>
2020-04-15 19:11:56 +0200

    apply various fixes

--- scripts/template_configmap.sh
+++ scripts/template_configmap.sh
@@ -2,9 +2,13 @@
 
 # replace the pubsub project, topic, and subscription with their respective substitutions
 
+project=$1
+topic=$2
+subscription=$3
+
 template=`cat "kubernetes-manifests/bot/configmap.yaml" | 
-sed "s/{{_PUBSUB_PROJECT}}/$_PUBSUB_PROJECT/g" |
-sed "s/{{_PUBSUB_TOPIC}}/$_PUBSUB_TOPIC/g" |
-sed "s/{{_PUBSUB_SUBSCRIPTION}}/$_PUBSUB_SUBSCRIPTION/g"`
+sed "s/{{_PUBSUB_PROJECT}}/$project/g" |
+sed "s/{{_PUBSUB_TOPIC}}/$topic/g" |
+sed "s/{{_PUBSUB_SUBSCRIPTION}}/$subscription/g"`
 
 echo "$template" > kubernetes-manifests/bot/configmap.yaml
